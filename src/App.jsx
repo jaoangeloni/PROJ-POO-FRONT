767b601536dca0fa0b44baf433e9e8d44ee82df0
jaoangeloni <joaopedroangeloni@gmail.com>
2024-11-05 22:23:00 -0300

    feat: example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './styles/App.css';
+import Card from './components/card';
 
 function App() {
+  const [users, setUsers] = useState({})
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+        const response = await fetch('http://localhost:8080/users').then(res => res?.json())
+        setUsers(response);
+    }
+    fetchUsers();
+  },[])
+
   return (
-    <div className="">
-      a
+    <div className="flex flex-col w-screen h-screen">
+      {users?.map((user) => {
+        return(
+          <div className='bg-red-100 border border-blue-500'>
+            <div>{user.id}</div>
+            <div>{user.name}</div>
+            <div>{user.email}</div>
+          </div>
+        )
+      })}
     </div>
   );
 }
---
+++ src/components/card.jsx
@@ -0,0 +1,3 @@
+export default function Card(){
+    return <div className="h-12 w-full bg-red-500">asdasdasdasdadasd</div>
+}
